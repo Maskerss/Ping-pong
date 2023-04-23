311eea257ceb9eaa7fc712f87f1dfa4e5e40f6b1
Maskerss <105439079+Maskerss@users.noreply.github.com>
2023-04-23 12:01:24 +0300

    Add files via upload

--- Ping-pong.py
+++ Ping-pong.py
@@ -10,7 +10,8 @@ class GameSprite(sprite.Sprite):
        # каждый спрайт должен хранить свойство image - изображение
        self.image = transform.scale(image.load(player_image), (size_x, size_y))
        self.imagee = player_image
-       self.speed = player_speed
+       self.speed_x = player_speed
+       self.speed_y = player_speed
        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
        self.rect = self.image.get_rect()
        self.rect.x = player_x
@@ -22,19 +23,32 @@ class GameSprite(sprite.Sprite):
 
 class Ball(GameSprite):
     def move(self):
-        if self.rect.x < 650:    
-            if self.rect.x > 0 and self.rect.y > 0:
-                self.rect.x += self.speed
-                self.rect.y -= self.speed
-            if self.rect.x > 0 and self.rect.y == 0:
-                self.rect.x += self.speed
-                self.rect.y += self.speed
-        
+        if self.rect.x > 0 and self.rect.x != 650:
+            self.rect.x += self.speed_x
+            self.rect.y -= self.speed_y
+        if self.rect.y == 0:
+            self.speed_x *= 1
+            self.speed_y *= -1
+        if self.rect.y == 450:
+            self.speed_x *= 1
+            self.speed_y *= -1
+               
+        if sprite.collide_rect(myach, rock1):
+            self.speed_x *= -1
+        if sprite.collide_rect(myach, rock2):
+            self.speed_x *= -1
 
+class Rocket(GameSprite):
+    def move(self, k1, k2):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[k1] and self.rect.y < 400 :
+            self.rect.y += self.speed_y
+        if keys_pressed[k2] and  self.rect.y > 0:
+            self.rect.y -= self.speed_y
 
 myach = Ball('zelya.png', 350, 250, 50, 50, 5)
-
-
+rock1 = Rocket('rus.png', 650, 250, 20, 100, 6)
+rock2 = Rocket('nato.png', 50, 250, 20, 100, 6)
 #Игровая сцена:
 win_width = 700
 win_height = 500
@@ -53,11 +67,11 @@ while game:
        
     if finish != True:
         window.blit(background,(0, 0))
-        
         myach.reset()
         myach.move()
-
-
-
+        rock1.reset()
+        rock1.move(K_DOWN, K_UP)
+        rock2.reset()
+        rock2.move(K_s, K_w)
         clock.tick(FPS)
         display.update()
